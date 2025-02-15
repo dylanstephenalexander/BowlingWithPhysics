d74d2693825f27c5622a0b04e3940bd902aa248b
dylanstephenalexander <dylanstephenalexander@gmail.com>
2025-02-14 22:31:54 -0800

    basic bowling and pins setup

---
+++ Assets/BallController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private float force = 1f;
+    [SerializeField] private InputManager inputManager;
+    private bool isBallLaunched;
+    private Rigidbody ballRB;
+    void Start()
+    {
+        ballRB = GetComponent<Rigidbody>();
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
+    }
+    private void LaunchBall(){
+        if(isBallLaunched)
+            return;
+        isBallLaunched = true;
+        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+}
